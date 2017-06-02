a61e52e603b6d5c50b5689f864669434c02e0dde
Maxim Teryokhin <max.teryokhin@gmail.com>
2017-06-02 09:21:54 +0300

    Some code optimizations

--- student_bsu_by/student.py
+++ student_bsu_by/student.py
@@ -1,4 +1,5 @@
 import requests
+import re
 import os
 from .exceptions import NotImplementedYet, InvalidCaptchaError, InvalidCredentialsError
 
@@ -9,24 +10,17 @@ class Student:
 
     def __init__(self, surname, student_id='', contract_num='', captcha_solver=None):
         if not student_id and not contract_num:
-            raise AttributeError("Either student id or contract number should be specified")
+            raise AttributeError("Either student id or contract number must be specified")
         self.surname = str(surname)
         self.student_id = str(student_id)
         self.contract_num = str(contract_num)
         self.captcha_solver = captcha_solver
         self._s = requests.session()
 
+        self.logged_in = False
         self._login()
 
     def _login(self):
-        def extract_between(text, text_begin, text_end, do_not_include_begin=False):
-            pos_begin = text.find(text_begin)
-            pos_end = text.find(text_end, pos_begin + len(text_begin))
-            if do_not_include_begin:
-                return text[pos_begin + len(text_begin):pos_end]
-            else:
-                return text[pos_begin:pos_end]
-
         self._s.get(self._DOMAIN + "/Login.aspx")
 
         # Save captcha_image
@@ -35,6 +29,7 @@ class Student:
             for block in captcha_image.iter_content(1024):
                 handle.write(block)
 
+        # Solve captcha
         if self.captcha_solver:
             captcha_result = self.captcha_solver(os.path.abspath(self._CAPTCHA_FILENAME))
         else:
@@ -42,8 +37,8 @@ class Student:
 
         # Make auth request
         auth_data = {"tbFam": self.surname,
-                     "tbNumStud": "" if not self.student_id else self.student_id,
-                     "tbNumDogovor": "" if not self.contract_num else self.contract_num,
+                     "tbNumStud": self.student_id,
+                     "tbNumDogovor":  self.contract_num,
                      "TextBox1": str(captcha_result),
                      # Some magic data below
                      "Button1": "Вход",
@@ -53,26 +48,35 @@ class Student:
                      "__EVENTTARGET": "",
                      "__EVENTARGUMENT": ""
                      }
-
         result_html = self._s.post(self._DOMAIN + "/Login.aspx", data=auth_data).text
+
+        # Check for errors
         if "lError" in result_html:
-            error_msg = extract_between(result_html, '<font color="Red">', "</font>", True)
+            error_msg = re.search('<font color="Red">(.*?)</font>', result_html).group(1)
             raise InvalidCredentialsError(error_msg)
         elif "Label6" in result_html:
-            error_msg = extract_between(result_html, '<font color="Red">', "</font>", True)
+            error_msg = re.search('<font color="Red">(.*?)</font>', result_html).group(1)
             raise InvalidCaptchaError(error_msg)
         elif "Выход" in result_html:
-            print("Successfully logged in!")
+            self.logged_in = True
 
     def get_term_data(self, term=0):
+        if not self.logged_in:
+            self._login()
         raise NotImplementedYet()
 
     def get_general_data(self):
+        if not self.logged_in:
+            self._login()
         raise NotImplementedYet()
 
     def get_debt_data(self):
+        if not self.logged_in:
+            self._login()
         raise NotImplementedYet()
 
     def get_credentials(self):
+        if not self.logged_in:
+            self._login()
         raise NotImplementedYet()
 
